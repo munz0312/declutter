95ba234dd242434b32c2a0015172784387c44b4a
Munzir Mohd Al Attas <mohdalattasmunzir@gmail.com>
2025-12-13 02:13:09 +0000

    update dependencies

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+import ollama
+
+
 def main():
-    print("Hello from declutter!")
+    client = ollama.Client(host='http://192.168.1.131:11434')
+    response = client.chat(
+        model='qwen2.5vl:3b',
+        messages=[{
+            'role': 'user',
+            'content': 'What is 10 + 10'
+        }]
+    )
+
+    print(response['message']['content'])
 
 
 if __name__ == "__main__":
